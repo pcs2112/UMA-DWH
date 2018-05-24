cf90395782d11cc62c7c7b3ff896a44fb3a68631
Vincent Chavez <vchavez92780@gmail.com>
2018-05-24 12:55:41 -0400

    Added validation functions for new and existing users

--- src/routes/Users/List/UserForm/validate.js
+++ src/routes/Users/List/UserForm/validate.js
@@ -1,12 +1,19 @@
 import memoize from 'lru-memoize';
 import { createValidator, required, email } from 'javascript-utils/lib/validation';
 
-const validator = createValidator({
+export const newUserValidator = memoize(10)(createValidator({
+  employee_first_name: required(),
+  employee_last_name: required(),
+  employee_email: [required(), email()],
+  employee_phone: required(),
+  employee_cell_phone: required(),
+  employee_password: required()
+}));
+
+export const existingUserValidator = memoize(10)(createValidator({
   employee_first_name: required(),
   employee_last_name: required(),
   employee_email: [required(), email()],
   employee_phone: required(),
   employee_cell_phone: required()
-});
-
-export default memoize(10)(validator);
+}));
