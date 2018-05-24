2df7e4e61f814c0a21656b7cb67368c7c53fa2db
Vincent Chavez <vchavez92780@gmail.com>
2018-05-24 19:53:00 -0400

    Handle Stored procedure exception during login

--- uma_dwh/users/views.py
+++ uma_dwh/users/views.py
@@ -2,6 +2,8 @@ from flask import Blueprint, jsonify, request
 from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required,
                                 get_jwt_identity, get_jwt_claims)
 from uma_dwh.exceptions import InvalidUsage
+from uma_dwh.db.exceptions import SPException
+from uma_dwh.db.etl import fetch_error
 from uma_dwh.db.users import fetch_users, fetch_user_by_email, login_user
 
 
@@ -22,7 +24,11 @@ def get_current_user():
 @blueprint.route('/api/users/login', methods=('POST',))
 def post_login():
     body = request.get_json(silent=True)
-    result = login_user(body['email'], body['password'])
+    try:
+        result = login_user(body['email'], body['password'])
+    except SPException as e:
+        raise InvalidUsage.etl_error(e.message, fetch_error(e.error_id))
+
     if result == -1:
         raise InvalidUsage.form_validation_error('Invalid account.', {'email': 'Invalid account.'})
 
