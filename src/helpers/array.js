9e61d9f6bb8800cbc56017ea0b9103e6db49c735
Vincent Chavez <vchavez92780@gmail.com>
2018-05-24 21:26:49 -0400

    Added function to replace an object in a list

--- src/helpers/array.js
+++ src/helpers/array.js
@@ -1,8 +1,28 @@
 /**
- * Finds the index of an item in a list.
+ * Finds the index of an object in a list. -1 is
+ * returned if the object is not found.
  *
- * @param {Array} list
+ * @param {Array<Object>} list
  * @param {String|Number} value
  * @param {String} key
+ * @returns {Number}
  */
-export const findItemIndexByValue = (list, value, key = 'id') => list.findIndex(item => item[key] === value);
+export const findObjIndexByValue = (list, value, key = 'id') => list.findIndex(item => item[key] === value);
+
+/**
+ * Replaces an object in a list and returns the new list.
+ *
+ * @param {Array<Object>} list
+ * @param {Object} item
+ * @param {String|Number} value
+ * @param {String} key
+ * @returns {undefined|Array<Object>}
+ */
+export const replaceObjByValue = (list, item, value, key = 'id') => {
+  const index = findObjIndexByValue(list, value, key);
+  if (index < 0) {
+    return [...list];
+  }
+
+  return list.splice(index, 1, item);
+};
