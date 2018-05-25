cf895a0c1ba717429b6e7ff6dc994d5f2e12f869
Vincent Chavez <vchavez92780@gmail.com>
2018-05-24 20:37:25 -0400

    Display server side error message in login form

--- src/routes/Login/LoginForm/LoginForm.js
+++ src/routes/Login/LoginForm/LoginForm.js
@@ -2,17 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { reduxForm, Field } from 'redux-form';
 import { Form, Button } from 'semantic-ui-react';
+import { isEmpty } from 'helpers/utils';
 import { TextField } from 'components/ReduxForm';
+import FormError from 'components/FormError';
 import validate from './validate';
 
 const LoginForm = ({
-  pristine, submitting, handleSubmit, onSubmit
+  pristine, submitting, error, handleSubmit, onSubmit
 }) => (
   <Form
     onSubmit={handleSubmit(onSubmit)}
     size="large"
     loading={submitting}
+    error={!isEmpty(error)}
   >
+    {error && <FormError error={error} />}
     <Field name="email" type="text" component={TextField} label="Email" required />
     <Field name="password" type="password" component={TextField} label="Password" required />
     <div className="field">
@@ -26,6 +30,7 @@ const LoginForm = ({
 LoginForm.propTypes = {
   submitting: PropTypes.bool,
   pristine: PropTypes.bool,
+  error: PropTypes.string,
   handleSubmit: PropTypes.func.isRequired,
   onSubmit: PropTypes.func.isRequired
 };
