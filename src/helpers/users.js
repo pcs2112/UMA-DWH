c5749363131147f8abbdcc946d9c657b9a229b65
Vincent Chavez <vchavez92780@gmail.com>
2018-05-25 22:05:17 -0400

    Added function to check if a user is the current user

--- src/helpers/users.js
+++ src/helpers/users.js
@@ -5,3 +5,12 @@
  * @returns {Boolean}
  */
 export const isAdmin = user => [1, 2].findIndex(item => user.id === item) !== undefined;
+
+/**
+ * Checks the specified user is the current application user.
+ *
+ * @param {Object} currentUser
+ * @param {Object} user
+ * @returns {boolean}
+ */
+export const isCurrentUser = (currentUser, user) => currentUser.id === user.id;
