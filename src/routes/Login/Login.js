ed3644579b646c34514d7643f98e89f487fb8c8d
Vincent Chavez <vchavez92780@gmail.com>
2018-05-27 13:19:35 -0400

    Wrapped login route in the guest layout

--- src/routes/Login/Login.js
+++ src/routes/Login/Login.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Grid, Header, Segment } from 'semantic-ui-react';
+import { Header, Segment } from 'semantic-ui-react';
 import userModule from 'redux/modules/user';
+import withGuestLayout from 'components/WithGuestLayout';
 import LoginForm from './LoginForm';
-import { mainGridStyles, mainColStyles } from './css';
 
 class Login extends Component {
   static propTypes = {
@@ -16,24 +16,22 @@ class Login extends Component {
     const { onLogin, onLoginSuccess } = this.props;
 
     return (
-      <Grid textAlign="center" verticalAlign="middle" style={mainGridStyles}>
-        <Grid.Column style={mainColStyles}>
-          <Header as="h1" textAlign="center">
-            Log-in to the UMA
-          </Header>
-          <Segment textAlign="left" color="grey">
-            <LoginForm
-              onSubmit={onLogin}
-              onSubmitSuccess={onLoginSuccess}
-            />
-          </Segment>
-        </Grid.Column>
-      </Grid>
+      <Fragment>
+        <Header as="h1" textAlign="center">
+          Log-in to the UMA
+        </Header>
+        <Segment textAlign="left" color="grey">
+          <LoginForm
+            onSubmit={onLogin}
+            onSubmitSuccess={onLoginSuccess}
+          />
+        </Segment>
+      </Fragment>
     );
   }
 }
 
-export default connect(
+export default withGuestLayout(connect(
   null,
   dispatch => ({
     onLogin: data => dispatch(userModule.actions.login(data)),
@@ -41,4 +39,4 @@ export default connect(
       dispatch(userModule.actions.fetchUser());
     }
   })
-)(Login);
+)(Login));
