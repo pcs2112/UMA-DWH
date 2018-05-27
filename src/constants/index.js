1200c1f14f6fd1b7418f17b1138574a3c9546077
Vincent Chavez <vchavez92780@gmail.com>
2018-05-27 15:52:10 -0400

    Added constants used for the scenario in the password reset flow

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,3 +3,7 @@ export const CURRENT_STATUS_INTERAL_DURATION = 15000;
 export const JWT_ACCESS_TOKEN_KEY = 'jwt_dwh_access_token';
 export const JWT_REFRESH_TOKEN_KEY = 'jwt_dwh_refresh_token';
 export const DEFAULT_FORM_ERROR = 'Validation error.';
+export const FORGOT_PASSWORD_SCENARIO = 1;
+export const FORGOT_PASSWORD_VERIFICATION_CODE_SCENARIO = 2;
+export const FORGOT_PASSWORD_RESET_SCENARIO = 3;
+export const FORGOT_PASSWORD_SUCCESS_SCENARIO = 4;
