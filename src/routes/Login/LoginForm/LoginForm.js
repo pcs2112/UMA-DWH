7cb4a26bd020255effe883e56497328f32283b5f
Vincent Chavez <vchavez92780@gmail.com>
2018-05-27 16:57:20 -0400

    Added link to the forgot password page

--- src/routes/Login/LoginForm/LoginForm.js
+++ src/routes/Login/LoginForm/LoginForm.js
@@ -1,7 +1,8 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { reduxForm, Field } from 'redux-form';
-import { Button } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { Button, Divider } from 'semantic-ui-react';
 import withBasicForm from 'components/WithBasicForm';
 import { TextField } from 'components/ReduxForm';
 import validate from './validate';
@@ -17,6 +18,12 @@ const LoginForm = ({
         Sign in
       </Button>
     </div>
+    <Divider section />
+    <p className="centered-aligned">
+      <Link to="/forgot">
+        Forgot password
+      </Link>
+    </p>
   </Fragment>
 );
 
