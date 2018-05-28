6022a052cf696ecb2060ab6c972234cac166a2b7
Vincent Chavez <vchavez92780@gmail.com>
2018-05-28 16:14:50 -0400

    Removed unused validation file

--- src/routes/Users/List/UserForm/validate.js
+++ src/routes/Users/List/UserForm/validate.js
@@ -1,20 +1,22 @@
 import memoize from 'lru-memoize';
-import { createValidator, required, email } from 'javascript-utils/lib/validation';
-import { phoneNumber } from 'helpers/validation';
+import { createValidator, required, email, validPassword, numericLength } from 'javascript-utils/lib/validation';
+
+const phoneNumberValidator = numericLength(10, 'Please enter a valid phone number.', true);
 
 export const newUserValidator = memoize(10)(createValidator({
   employee_first_name: required(),
   employee_last_name: required(),
-  employee_email: [required(), email()],
-  employee_phone: phoneNumber(10, 'Enter a valid phone number.'),
-  employee_cell_phone: phoneNumber(10, 'Enter a valid cellphone number.'),
-  employee_password: required()
+  employee_email: email(),
+  employee_phone: phoneNumberValidator,
+  employee_cell_phone: phoneNumberValidator,
+  employee_password: validPassword(1, 1, 8)
 }));
 
 export const existingUserValidator = memoize(10)(createValidator({
   employee_first_name: required(),
   employee_last_name: required(),
-  employee_email: [required(), email()],
-  employee_phone: phoneNumber(10, 'Enter a valid phone number.'),
-  employee_cell_phone: phoneNumber(10, 'Enter a valid cellphone number.')
+  employee_email: email(),
+  employee_phone: phoneNumberValidator,
+  employee_cell_phone: phoneNumberValidator,
+  employee_password: validPassword(1, 1, 8, undefined, true)
 }));
