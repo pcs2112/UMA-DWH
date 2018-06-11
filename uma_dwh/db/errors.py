313e5efb2964b79d18a629ebd8f46092192e83c1
Vincent Chavez <vchavez92780@gmail.com>
2018-06-11 00:10:35 -0400

    Added function to update and error resolution file

--- uma_dwh/db/errors.py
+++ uma_dwh/db/errors.py
@@ -1,9 +1,9 @@
 from .mssql_db import execute_sp
 
 
-def save_error_resolution_file(description, file_path_filename):
+def insert_error_resolution_file(description, file_path_filename):
     """
-    Stores an error type resolution file.
+    Creates an error type resolution file record.
     :param description: File description
     :param file_path_filename: Full file path with file name
     :type description: str
@@ -14,6 +14,28 @@ def save_error_resolution_file(description, file_path_filename):
       {
         'message': 'save',
         'ID': '',
+        'ACTIVE_FLAG': '1',
+        'DESCRIPTION': description,
+        'FILE_PATH_FILENAME': file_path_filename
+      }
+    )
+
+
+def update_error_resolution_file(_id, description, file_path_filename):
+    """
+    Updates an error type resolution file.
+    :param _id: File ID
+    :param description: File description
+    :param file_path_filename: Full file path with file name
+    :type _id: int
+    :type description: str
+    :type file_path_filename: str
+    """
+    execute_sp(
+      'MWH.ERROR_RESOLUTIONS_MGR',
+      {
+        'message': 'save',
+        'ID': str(_id),
         'ACTIVE_FLAG': '1',
         'DESCRIPTION': description,
         'FILE_PATH_FILENAME': file_path_filename
