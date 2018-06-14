1e7adf543d568b2b024b35b1ffeb8a102163bfe2
Vincent Chavez <vchavez92780@gmail.com>
2018-06-14 18:35:34 -0400

    Updated reducers config

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,24 +1,26 @@
 import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
 import { reducer as modal } from 'redux-modal';
+import errorTypeResolution from './modules/errorTypeResolution';
 import etlControlManager from './modules/etlControlManager';
 import etlCurrentStatus from './modules/etlCurrentStatus';
 import etlProcedureHistory from './modules/etlProcedureHistory';
 import etlCycleHistory from './modules/etlCycleHistory';
 import etlRunCheck from './modules/etlRunCheck';
 import etlServers from './modules/etlServers';
-import users from './modules/users';
 import user from './modules/user';
+import users from './modules/users';
 
 export default combineReducers({
   form,
   modal,
+  errorTypeResolution: errorTypeResolution.reducer,
   etlControlManager: etlControlManager.reducer,
   etlCurrentStatus: etlCurrentStatus.reducer,
   etlCycleHistory: etlCycleHistory.reducer,
   etlProcedureHistory: etlProcedureHistory.reducer,
   etlRunCheck: etlRunCheck.reducer,
   etlServers: etlServers.reducer,
-  users: users.reducer,
-  user: user.reducer
+  user: user.reducer,
+  users: users.reducer
 });
