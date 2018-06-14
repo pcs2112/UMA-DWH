c15f229b9452fd7ddf152d8c45bad04533d75808
Vincent Chavez <vchavez92780@gmail.com>
2018-06-14 18:50:22 -0400

    Added DB schema for the error type files

---
+++ uma_dwh/db/schemas/error_type_resolution/__init__.py
@@ -0,0 +1,1 @@
+from .files import schema as files
---
+++ uma_dwh/db/schemas/error_type_resolution/files.py
@@ -0,0 +1,9 @@
+schema = (
+  'id',
+  'insert_dttm',
+  'update_dttm',
+  'lst_mod_user',
+  'active_flag',
+  'description',
+  'file_path_filename'
+)
