e50f09a1f1cde6160f16ff0ae9dcad17ad475c2d
Vincent Chavez <vchavez92780@gmail.com>
2018-06-14 18:34:53 -0400

    Added Redux form component Field for getting upload file info

--- src/components/ReduxForm/index.js
+++ src/components/ReduxForm/index.js
@@ -1,4 +1,5 @@
 export CheckBoxField from './CheckBoxField';
+export FilePathField from './FilePathField';
 export RadioField from './RadioField';
 export SelectField from './SelectField';
 export TextAreaField from './TextAreaField';
