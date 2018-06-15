5788910f73b6d4aabe50d49b4845e3586df42b4d
Vincent Chavez <vchavez92780@gmail.com>
2018-06-14 20:04:00 -0400

    Fixed error creating a file

--- uma_dwh/error_type_resolution/views.py
+++ uma_dwh/error_type_resolution/views.py
@@ -16,19 +16,20 @@ def get_files():
 
 @blueprint.route('/api/error_type_resolution/files', methods=('POST',))
 @jwt_required
-def post_file():
+def post_create_file():
     body = request.get_json(silent=True)
+    print(body)
     try:
-        return jsonify(create_file(body.description, body.file_path_filename))
+        return jsonify(create_file(body['description'], body['file_path_filename']))
     except DBException:
         raise InvalidUsage.etl_error(message='Error creating file.')
 
 
 @blueprint.route('/api/error_type_resolution/files/<file_id>', methods=('POST',))
 @jwt_required
-def post_file(file_id):
+def post_update_file(file_id):
     body = request.get_json(silent=True)
     try:
-        return jsonify(update_file(file_id, body.description, body.file_path_filename))
+        return jsonify(update_file(file_id, body['description'], body['file_path_filename']))
     except DBException:
         raise InvalidUsage.etl_error(message='Error updating file.')
