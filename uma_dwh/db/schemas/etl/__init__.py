ba91c040f2c4826b0d0a4cb15a0fe57fd2915d0e
Vincent Chavez <vchavez92780@gmail.com>
2018-06-18 22:43:24 -0400

    Renamed schema file

--- uma_dwh/db/schemas/etl/__init__.py
+++ uma_dwh/db/schemas/etl/__init__.py
@@ -2,7 +2,7 @@ from .control_manager import schema as control_manager_schema
 from .current_cycle_status import schema as current_cycle_status_schema
 from .current_status import schema as current_status_schema
 from .cycle_history import schema as cycle_history_schema
-from .powerbi_history import schema as powerbi_history_schema
+from .powerbi_report_history import schema as powerbi_report_history_schema
 from .procedure_history import schema as procedure_history_schema
 from .server_db_procedures import schema as server_db_procedures_schema
 from .server_dbs import schema as server_dbs_schema
