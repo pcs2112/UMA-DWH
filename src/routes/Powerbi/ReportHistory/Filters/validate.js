c27a517a0aedf093b0e69828bc23139010508655
Vincent Chavez <vchavez92780@gmail.com>
2018-06-20 14:37:04 -0400

    Updated date validation

--- src/routes/Powerbi/ReportHistory/Filters/validate.js
+++ src/routes/Powerbi/ReportHistory/Filters/validate.js
@@ -2,7 +2,7 @@ import memoize from 'lru-memoize';
 import { createValidator, validDate, validDateRange } from 'javascript-utils/lib/validation';
 import { DEFAULT_DATE_FORMAT } from 'constants/index';
 
-const validDateValidator = validDate(DEFAULT_DATE_FORMAT);
+const validDateValidator = validDate(DEFAULT_DATE_FORMAT, undefined, true);
 const validDateRangeValidator = validDateRange('start_date', DEFAULT_DATE_FORMAT);
 
 const validator = createValidator({
