6f91c3ee695055b642e5a7c4cdaa3345e121dc82
Vincent Chavez <vchavez92780@gmail.com>
2018-06-20 00:17:34 -0400

    Added selector to get the Power BI filters

--- src/redux/modules/powerbiReportHistory/selectors.js
+++ src/redux/modules/powerbiReportHistory/selectors.js
@@ -1,13 +1,29 @@
+import { createSelector } from 'reselect';
+import { objectHasOwnProperty } from 'javascript-utils/lib/utils';
 import {
   createDataSelector,
   createFetchingErrorSelector,
   createGetItemsSelector
 } from 'helpers/selectors';
 
+const emptyFilters = {
+  startDate: '',
+  endDate: ''
+};
+
 /**
  * Returns the item list from the state.
  */
 const _getData = createDataSelector('powerbiReportHistory');
+
+/**
+ * Returns the filters from the state.
+ * @param {Object} state
+ */
+const _getFilters = state => (objectHasOwnProperty(state.powerbiReportHistory, 'startDate') ? {
+  startDate: state.powerbiReportHistory.startDate,
+  endDate: state.powerbiReportHistory.endDate
+} : emptyFilters);
 
 /**
  * Returns the fetching error.
@@ -19,3 +35,11 @@ export const getFetchingError = createFetchingErrorSelector('powerbiReportHistor
  * Selector to get the Power BI report history.
  */
 export const getPowerbiReportHistory = createGetItemsSelector(_getData);
+
+/**
+ * Selector to get the Power BI report history filters.
+ */
+export const getFilters = createSelector(
+  [_getFilters],
+  filtersFromState => filtersFromState
+);
