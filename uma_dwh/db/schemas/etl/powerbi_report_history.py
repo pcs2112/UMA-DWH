0709faadbff4a31276299277da78ef620b86605e
Vincent Chavez <vchavez92780@gmail.com>
2018-08-02 15:35:14 -0400

    Updated Power BI report history

--- uma_dwh/db/schemas/etl/powerbi_report_history.py
+++ uma_dwh/db/schemas/etl/powerbi_report_history.py
@@ -1,11 +1,13 @@
 schema = (
   'id',
-  'report_name',
+  'stored_procedure',
   'from_dttm',
   'to_dttm',
   'started',
   'finished',
   'run_time',
   'rows_returned',
-  'error_message'
+  'error_message',
+  'source_table_name',
+  'report_name'
 )
