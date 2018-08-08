eae721a2a3041e94179149f940e5d04dddf149de
Vincent Chavez <vchavez92780@gmail.com>
2018-08-07 23:48:00 -0400

    Added selector creators for cycle group pagination

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -44,3 +44,74 @@ export const createGetItemByIdSelector = (getItemsSelector, getItemIdSelector) =
   [getItemsSelector, getItemIdSelector],
   (items, itemId) => (itemId ? items.find(item => item.id === itemId) : undefined)
 );
+
+/**
+ * Creates a selector to return the pagination's current cycle group.
+ */
+export const createGetCurrentCycleGroup = (stateName, propertyName = 'currentCycleGroup') =>
+  state => state[stateName][propertyName] || 0;
+
+/**
+ * Creates a selector to return the pagination's current cycle group's start dttm.
+ */
+export const createGetCurrentCycleGroupStartDttm = (getItemsSelector, startDttmKey = 'start_dttm') =>
+  createSelector(
+    [getItemsSelector],
+    data => (data.length > 0 ? data[0][startDttmKey] : 'N/A')
+  );
+
+/**
+ * Returns the prev cycle group's pagination data.
+ */
+export const createGetPrevCycleGroupStartData = (getItemsSelector, getCurrentCycleGroupSelector, pageSize = 1000) =>
+  createSelector(
+    [getItemsSelector, getCurrentCycleGroupSelector],
+    (data, currentCycleGroup) => {
+      if (data.length < 1) {
+        return {
+          currentCycleGroup: 0,
+          fromNum: '',
+          toNum: ''
+        };
+      }
+
+      const newCycleGroup = currentCycleGroup < 1 ? 0 : currentCycleGroup - 1;
+      let fromNum = '';
+      let toNum = '';
+      if (newCycleGroup > 0) {
+        fromNum = data[0].id - pageSize;
+        toNum = fromNum + pageSize;
+      }
+      return {
+        currentCycleGroup: newCycleGroup,
+        fromNum,
+        toNum
+      };
+    }
+  );
+
+/**
+ * Returns the next cycle group's pagination data.
+ */
+export const createGetNextCycleGroupStartData = (getItemsSelector, getCurrentCycleGroupSelector, pageSize = 1000) =>
+  createSelector(
+    [getItemsSelector, getCurrentCycleGroupSelector],
+    (data, currentCycleGroup) => {
+      if (data.length < 1) {
+        return {
+          currentCycleGroup: 0,
+          fromNum: '',
+          toNum: ''
+        };
+      }
+
+      const newCycleGroup = currentCycleGroup + 1;
+      const fromNum = data[data.length - 1].id + pageSize;
+      const toNum = fromNum + pageSize;
+      return {
+        currentCycleGroup: newCycleGroup,
+        fromNum,
+        toNum
+      };
+    }
+  );
