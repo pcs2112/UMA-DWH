4a9e7af99e9b1568205c8a7fea3f60f16f80db85
Vincent Chavez <vchavez92780@gmail.com>
2018-08-07 23:52:03 -0400

    Added action creators to pagination the report runs

--- src/redux/modules/powerbiReportRuns/actions.js
+++ src/redux/modules/powerbiReportRuns/actions.js
@@ -1,3 +1,5 @@
+import { getPrevCycleGroupStartData, getNextCycleGroupStartData } from './selectors';
+
 export const actionTypes = {
   FETCH_BEGIN: 'powerbiReportRuns/FETCH_BEGIN',
   FETCH_SUCCESS: 'powerbiReportRuns/FETCH_SUCCESS',
@@ -8,7 +10,7 @@ export const actionTypes = {
 /**
  * Action to the Power BI report runs.
  */
-export const fetchPowerbiReportRuns = reportName => ({
+export const fetchPowerbiReportRuns = (reportName, cycleGroup = 0, fromNum = '', toNum = '') => ({
   types: [
     actionTypes.FETCH_BEGIN,
     actionTypes.FETCH_SUCCESS,
@@ -16,13 +18,36 @@ export const fetchPowerbiReportRuns = reportName => ({
   ],
   makeRequest: client => client.get('/api/etl/powerbi_report_runs', {
     params: {
-      report_name: reportName
+      report_name: reportName,
+      from_num: fromNum,
+      to_num: toNum
     }
   }),
   payload: {
-    reportName
+    reportName,
+    currentCycleGroup: cycleGroup
   }
 });
+
+/**
+ * Action to fetch the previous cycle group before the current cycle group.
+ */
+export const fetchPrev = () => (dispatch, getState) => {
+  const state = getState();
+  const { currentCycleGroup, fromNum, toNum } = getPrevCycleGroupStartData(state);
+  const { reportName } = state.powerbiReportRuns;
+  dispatch(fetchPowerbiReportRuns(reportName, currentCycleGroup, fromNum, toNum));
+};
+
+/**
+ * Action to fetch the next cycle group after the current cycle group.
+ */
+export const fetchNext = () => (dispatch, getState) => {
+  const state = getState();
+  const { currentCycleGroup, fromNum, toNum } = getNextCycleGroupStartData(state);
+  const { reportName } = state.powerbiReportRuns;
+  dispatch(fetchPowerbiReportRuns(reportName, currentCycleGroup, fromNum, toNum));
+};
 
 /**
  * Resets the state.
