8f3178d34644ef0655d1f67189e087700ff3190d
Vincent Chavez <vchavez92780@gmail.com>
2018-09-09 23:14:08 -0400

    Fixed formatting dates

--- uma_dwh/json.py
+++ uma_dwh/json.py
@@ -12,5 +12,7 @@ class JSONEnhanced(JSONEncoder):
             return float(obj)
         elif isinstance(obj, datetime):
             return obj.strftime('%Y-%m-%d %H:%M:%S')
+        elif isinstance(obj, date):
+            return obj.strftime('%Y-%m-%d')
         else:
             return JSONEncoder.default(self, obj)
