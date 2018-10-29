bdd70329cf0c2046b92ec752bdcb74f06caa5a92
Vincent Chavez <vchavez92780@gmail.com>
2018-10-29 00:44:04 -0400

    Fixed menu item url

--- src/components/MainMenu/MainMenu.js
+++ src/components/MainMenu/MainMenu.js
@@ -37,7 +37,7 @@ class MainMenu extends Component {
         <Menu.Item as={Link} to="/reports/history" active={isActive(pathname, '/reports/history')}>
           DWH Report History
         </Menu.Item>
-        <Menu.Item as={Link} to="/dwh/statistics" active={isActive(pathname, '/dwh/statistics')}>
+        <Menu.Item as={Link} to="/statistics/history" active={isActive(pathname, '/statistics/history')}>
           DWH Statistics History
         </Menu.Item>
         <Menu.Item as={Link} to="/dwh/errors" active={isActive(pathname, '/dwh/errors')}>
