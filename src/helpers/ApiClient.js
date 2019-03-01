bd855fb7967ee8f81a371d1efe51bf737dcb6e6b
Vicente Chavez <vchavez92780@gmail.com>
2019-03-01 09:57:57 -0500

    Added functionality to download a file

--- src/helpers/ApiClient.js
+++ src/helpers/ApiClient.js
@@ -6,7 +6,7 @@ const formatUrl = path => (path[0] !== '/' ? `/${path}` : path);
 
 class ApiClient {
   constructor() {
-    methods.forEach((method) => // eslint-disable-line
+    methods.forEach((method) => {
       this[method] = (path, { headers = {}, params = {}, data } = {}) => new Promise((resolve, reject) => {
         const request = superagent[method](formatUrl(path));
 
@@ -25,12 +25,51 @@ class ApiClient {
           request.send(data);
         }
 
-        request.end((err, { body } = {}) => err ? reject(body || err) : resolve(body)); // eslint-disable-line
-      }));
+        request.end((err, { body } = {}) => {
+          if (err) {
+            return reject(body || err);
+          }
+
+          return resolve(body);
+        });
+      });
+    });
   }
 
   setAccessToken(accessToken) {
     this.accessToken = accessToken;
+  }
+
+  downloadFile = (path, { headers = {}, params = {}, data } = {}) => {
+    const request = superagent.post(formatUrl(path));
+
+    // Set Access Token
+    if (this.accessToken) {
+      headers.Authorization = `Bearer ${this.accessToken}`;
+    }
+
+    // Set headers
+    request.set(headers);
+
+    // Set request params
+    request.query(params);
+
+    if (data) {
+      request.send(data);
+    }
+
+    return request.responseType('blob')
+      .then((res) => {
+        const url = window.URL.createObjectURL(new Blob([res.data]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', params.fileName);
+        document.body.appendChild(link);
+        link.click();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 }
 
