a44a1b1d6b2ef466b27dc3fad3bfb5d7cb6d3f09
Vicente Chavez <vchavez92780@gmail.com>
2019-03-06 21:30:39 -0500

    Added code to format dates

--- uma_dwh/db/college_scorecard.py
+++ uma_dwh/db/college_scorecard.py
@@ -1,24 +1,114 @@
 import xlwt
 import io
 from .mssql_db import execute_sp
-from uma_dwh.utils import is_float, is_int, list_chunks
+from uma_dwh.utils import is_float, is_int, is_datetime, list_chunks
 
 
 def get_excel_export_data(columns, file_name):
+    wb = xlwt.Workbook(encoding='UTF-8')
+    
+    # Prints the title worksheet
+    ws_data = wb.add_sheet('TITLE')
+    print_ws_export_title(ws_data, columns, file_name)
+
+    # Prints the data worksheet
+    ws_data = wb.add_sheet('DATA')
+    print_ws_export_data(ws_data, columns, file_name)
+
+    output = io.BytesIO()
+    wb.save(output)
+    return output.getvalue()
+
+
+def print_ws_header(ws, header):
+    """ Print the header in the specified worksheet instance. """
+    for x, cell in enumerate(header):
+        ws.write(0, x, cell)
+        ws.col(x).width = (len(header[x]) + 4) * 367
+
+
+def print_ws_data(ws, rows):
+    """ Print the data in the specified worksheet instance. """
+    for i, row in enumerate(rows):
+        for x, cell in enumerate(row):
+            label = cell
+            if label is None or label == 'NULL':
+                label = ''
+            elif is_float(cell):
+                label = float(cell)
+            elif is_int(cell):
+                label = int(cell)
+            elif is_datetime(cell):
+                label = cell.strftime('%Y-%m-%d %H:%M:%S')
+
+            ws.write(i + 1, x, label)
+
+
+def print_ws_export_title(ws, columns, file_name):
+    """ Prints the title worksheet. """
     column_groups = list(list_chunks(columns, 10))
     header = []
     raw_data = []
 
     for cg_i, column_group in enumerate(column_groups):
         xml = '<COLUMNS>'
-        xml += f'<COLUMN NAME="INSTNM" />'
-        xml += f'<COLUMN NAME="OPEID" />'
-
+    
         for col in column_group:
             xml += f'<COLUMN NAME="{col}" />'
-
+    
         xml += '</COLUMNS>'
+    
+        data_result = execute_sp(
+            sp_name='MWH_FILES.MANAGE_CollegeScorecard_Console',
+            in_args={
+                'message': 'EXPORT',
+                'VARCHAR_01': 'TITLE PAGE',
+                'VARCHAR_02': file_name,
+                'VARCHAR_03': xml,
+                'VARCHAR_04': '',
+                'VARCHAR_05': '',
+                'VARCHAR_06': '',
+                'VARCHAR_07': '',
+                'VARCHAR_08': '',
+                'VARCHAR_09': ''
+            },
+            out_arg='TryCatchError_ID',
+            as_dict=False
+        )
+        
+        if len(data_result) < 1:
+            raise Exception('No title data returned in the call to the MWH_FILES.MANAGE_CollegeScorecard_Console SP.')
+        
+        if len(header) < 1:
+            for col in data_result[0]:
+                header.append(col.upper())
 
+        for row in data_result[1]:
+            raw_data.append(row)
+            
+    # Print the data tab header
+    print_ws_header(ws, header)
+
+    # Print rows
+    print_ws_data(ws, raw_data)
+    
+  
+def print_ws_export_data(ws, columns, file_name):
+    """ Prints the data worksheet. """
+    column_groups = list(list_chunks(columns, 10))
+    header = []
+    raw_data = []
+    
+    for cg_i, column_group in enumerate(column_groups):
+        xml = '<COLUMNS>'
+        xml += f'<COLUMN NAME="INSTNM" />'
+        xml += f'<COLUMN NAME="OPEID" />'
+        
+        for col in column_group:
+            xml += f'<COLUMN NAME="{col}" />'
+        
+        xml += '</COLUMNS>'
+        
         data_result = execute_sp(
             sp_name='MWH_FILES.MANAGE_CollegeScorecard_Console',
             in_args={
@@ -36,54 +126,27 @@ def get_excel_export_data(columns, file_name):
             out_arg='TryCatchError_ID',
             as_dict=False
         )
-
+        
         if len(data_result) < 1:
             raise Exception('No title data returned in the call to the MWH_FILES.MANAGE_CollegeScorecard_Console SP.')
-
+        
         for j, col in enumerate(data_result[0]):
             if (cg_i == 0 and j == 0) or j > 2:
                 header.append(col.upper())
-
+        
         for j, raw_row in enumerate(data_result[1]):
             row = []
             for k, cell in enumerate(raw_row):
                 if (cg_i == 0 and k == 0) or k > 2:
                     row.append(cell)
-
+            
             if cg_i == 0:
                 raw_data.append(row)
             else:
                 raw_data[j].extend(row)
-
-    wb = xlwt.Workbook(encoding='UTF-8')
-    ws_data = wb.add_sheet('DATA')
-
+                
     # Print the data tab header
-    print_ws_header(ws_data, header)
+    print_ws_header(ws, header)
 
     # Print rows
-    print_ws_data(ws_data, raw_data)
-
-    output = io.BytesIO()
-    wb.save(output)
-    return output.getvalue()
-
-
-def print_ws_header(ws, header):
-    for x, cell in enumerate(header):
-        ws.write(0, x, cell)
-        ws.col(x).width = (len(header[x]) + 4) * 367
-
-
-def print_ws_data(ws, rows):
-    for i, row in enumerate(rows):
-        for x, cell in enumerate(row):
-            label = cell
-            if label is None or label == 'NULL':
-                label = ''
-            elif is_float(cell):
-                label = float(cell)
-            elif is_int(cell):
-                label = int(cell)
-
-            ws.write(i + 1, x, label)
+    print_ws_data(ws, raw_data)
--- uma_dwh/utils/__init__.py
+++ uma_dwh/utils/__init__.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 def date_diff_in_seconds(dt2, dt1):
     timedelta = dt2 - dt1
     return timedelta.days * 24 * 3600 + timedelta.seconds
@@ -21,6 +24,10 @@ def is_int(value):
         return False
     except ValueError:
         return False
+    
+    
+def is_datetime(value):
+    return isinstance(value, datetime.datetime)
 
 
 def list_chunks(l, n):
@@ -29,3 +36,8 @@ def list_chunks(l, n):
     for i in range(0, len(l), n):
         # Create an index range for l of n items:
         yield l[i:i+n]
+        
+        
+def format_datetime(date, format_str="%Y-%m-%d %H:%M"):
+    """ Formats the specified datetime. """
+    return date.strftime(format_str)
