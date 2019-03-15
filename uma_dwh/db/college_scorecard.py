a89982b5636289498d89edcce6c1dccfb576a39d
Vicente Chavez <vchavez92780@gmail.com>
2019-03-15 09:23:32 -0400

    Updated report  data returned

--- uma_dwh/db/college_scorecard.py
+++ uma_dwh/db/college_scorecard.py
@@ -52,7 +52,8 @@ def fetch_report(user_id, report_name):
     if not report:
         return None
 
-    report['xml_data'] = get_columns_from_xml(report['xml_data'])
+    report['columns'] = get_columns_from_xml(report['xml_data'])
+    del report['xml_data']
     return report
 
 
@@ -69,7 +70,8 @@ def fetch_report_by_id(id_, user_id):
       return None
     
     report = result[0]
-    report['xml_data'] = get_columns_from_xml(report['xml_data'])
+    report['columns'] = get_columns_from_xml(report['xml_data'])
+    del report['xml_data']
     return report
 
 
