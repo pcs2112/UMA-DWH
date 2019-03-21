18ec57a1ea52e8b40f5d43c281ec8c31b155bd18
Vincent Chavez <vchavez92780@gmail.com>
2019-03-21 12:44:20 -0400

    updated deployment script

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,8 @@ cp "$_cwd/requirements.txt" "$_targetRoot/$_rootDirName/requirements.txt"
 cp "$_cwd/uma_dwh_app.py" "$_targetRoot/$_rootDirName/uma_dwh_app.py"
 cp "$_cwd/.env.example" "$_targetRoot/$_rootDirName/.env"
 cp -r "$_cwd/uma_dwh" "$_targetRoot/$_rootDirName/uma_dwh"
+rm -rf "$_targetRoot/$_rootDirName/uma_dwh/static/run_books"
+mkdir  "$_targetRoot/$_rootDirName/uma_dwh/static/run_books"
 find "$_targetRoot/$_rootDirName" | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
 cd "$_targetRoot"
 zip -r "$_targetRoot/$_rootDirName.zip" ./${_rootDirName}/*
