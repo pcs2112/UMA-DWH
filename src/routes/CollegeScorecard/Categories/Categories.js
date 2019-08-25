92e154f83d66310179bc3b836a02b2494c935b70
Vicente Chavez <vchavez92780@gmail.com>
2019-08-25 10:26:36 -0400

    Added code to fetch the files

--- src/routes/CollegeScorecard/Categories/Categories.js
+++ src/routes/CollegeScorecard/Categories/Categories.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Segment, Button } from 'semantic-ui-react';
 import categoriesRdx from '../../../redux/modules/collegeScorecardCategories';
+import filesRdx from '../../../redux/modules/collegeScorecardFiles';
 import withMainLayout from '../../../components/WithMainLayout';
 import ListTable from './ListTable';
 import globalCss from '../../../css/global';
@@ -13,20 +14,20 @@ class Categories extends Component {
     categoriesDataLoaded: PropTypes.bool.isRequired,
     categoriesData: PropTypes.array.isRequired,
     categoriesFetchingError: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]).isRequired,
-    fetchCategories: PropTypes.func.isRequired,
-    resetCategories: PropTypes.func.isRequired,
+    fetchData: PropTypes.func.isRequired,
+    resetData: PropTypes.func.isRequired,
     onCreateCategoryClick: PropTypes.func.isRequired,
     onUpdateCategoryClick: PropTypes.func.isRequired
   };
 
   componentDidMount() {
-    const { fetchCategories } = this.props;
-    fetchCategories();
+    const { fetchData } = this.props;
+    fetchData();
   }
 
   componentWillUnmount() {
-    const { resetCategories } = this.props;
-    resetCategories();
+    const { resetData } = this.props;
+    resetData();
   }
 
   render() {
@@ -70,8 +71,15 @@ export default withMainLayout(connect(
     categoriesFetchingError: categoriesRdx.selectors.getFetchingError(state)
   }),
   dispatch => ({
-    fetchCategories: () => dispatch(categoriesRdx.actions.fetch()),
-    resetCategories: () => dispatch(categoriesRdx.actions.reset()),
+    fetchData: () =>
+      Promise.all([
+        dispatch(categoriesRdx.actions.fetch()),
+        dispatch(filesRdx.actions.fetch())
+      ]),
+    resetData: () => {
+      dispatch(categoriesRdx.actions.reset());
+      dispatch(filesRdx.actions.reset());
+    },
     onCreateCategoryClick: () => {
     },
     onUpdateCategoryClick: () => {
