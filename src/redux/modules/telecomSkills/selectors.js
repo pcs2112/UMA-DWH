26f01dd26c36cdf01b5f1ade1a39b85dface89a4
Devin Encalada <devinencalada@gmail.com>
2019-08-24 23:18:40 -0400

    altered selectors for telecom skills module

--- src/redux/modules/telecomSkills/selectors.js
+++ src/redux/modules/telecomSkills/selectors.js
@@ -1,18 +1,10 @@
-import { createSelector } from 'reselect';
 import {
   createDataSelector,
   createFetchingErrorSelector,
-  createGetItemsSelector,
-  createGetItemByIdSelector,
-  createGetPropertySelector
+  createGetItemsSelector
 } from 'javascript-utils/lib/selectors';
 
 const _getData = createDataSelector('errorTypeResolution', 'dataLoaded', 'data');
-
-/**
- * Returns the updating file id.
- */
-const _getUpdatingFileId = createGetPropertySelector('errorTypeResolution', 'updating');
 
 /**
  * Returns the error from the state.
@@ -24,26 +16,3 @@ export const getFetchingError = createFetchingErrorSelector('errorTypeResolution
  * Returns the Error type resolution data.
  */
 export const getFiles = createGetItemsSelector(_getData);
-
-/**
- * Returns the current updating file.
- */
-export const getUpdatingFile = createGetItemByIdSelector(_getData, _getUpdatingFileId);
-
-/**
- * Gets the initial form values for the updating file.
- */
-export const getUpdatingFileInitialValues = createSelector(
-  [getUpdatingFile],
-  (file) => {
-    if (!file) {
-      return {};
-    }
-
-    return {
-      id: file.id,
-      description: file.description,
-      file_path_filename: file.file_path_filename
-    };
-  }
-);
