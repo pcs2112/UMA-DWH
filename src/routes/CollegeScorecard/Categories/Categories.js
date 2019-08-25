574573743e83cf189a3c651a9f2be619899df483
Vicente Chavez <vchavez92780@gmail.com>
2019-08-25 10:14:48 -0400

    Added code to render the categories table

--- src/routes/CollegeScorecard/Categories/Categories.js
+++ src/routes/CollegeScorecard/Categories/Categories.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Segment } from 'semantic-ui-react';
+import { Segment, Button } from 'semantic-ui-react';
+import categoriesRdx from '../../../redux/modules/collegeScorecardCategories';
 import withMainLayout from '../../../components/WithMainLayout';
+import ListTable from './ListTable';
 import globalCss from '../../../css/global';
 
 class Categories extends Component {
   static propTypes = {
+    isCategoriesFetching: PropTypes.bool.isRequired,
+    categoriesDataLoaded: PropTypes.bool.isRequired,
+    categoriesData: PropTypes.array.isRequired,
+    categoriesFetchingError: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]).isRequired,
+    fetchCategories: PropTypes.func.isRequired,
+    resetCategories: PropTypes.func.isRequired,
+    onCreateCategoryClick: PropTypes.func.isRequired,
+    onUpdateCategoryClick: PropTypes.func.isRequired
   };
 
   componentDidMount() {
+    const { fetchCategories } = this.props;
+    fetchCategories();
   }
 
   componentWillUnmount() {
+    const { resetCategories } = this.props;
+    resetCategories();
   }
 
   render() {
+    const {
+      isCategoriesFetching,
+      categoriesDataLoaded,
+      categoriesData,
+      categoriesFetchingError,
+      onCreateCategoryClick,
+      onUpdateCategoryClick
+    } = this.props;
     return (
       <div>
         <Segment style={globalCss.pageHeaderSegment}>
@@ -23,8 +45,17 @@ class Categories extends Component {
             College Scorecard Category List
           </h1>
         </Segment>
+        <Segment>
+          <Button primary onClick={onCreateCategoryClick}>Create Category</Button>
+        </Segment>
         <Segment style={globalCss.pageHeaderSegment}>
-          Table here
+          <ListTable
+            isFetching={isCategoriesFetching}
+            dataLoaded={categoriesDataLoaded}
+            data={categoriesData}
+            fetchingError={categoriesFetchingError}
+            onEdit={onUpdateCategoryClick}
+          />
         </Segment>
       </div>
     );
@@ -32,6 +63,18 @@ class Categories extends Component {
 }
 
 export default withMainLayout(connect(
-  null,
-  null
+  state => ({
+    isCategoriesFetching: state.collegeScorecardCategories.isFetching,
+    categoriesDataLoaded: state.collegeScorecardCategories.dataLoaded,
+    categoriesData: categoriesRdx.selectors.getCollegeScorecardCategories(state),
+    categoriesFetchingError: categoriesRdx.selectors.getFetchingError(state)
+  }),
+  dispatch => ({
+    fetchCategories: () => dispatch(categoriesRdx.actions.fetch()),
+    resetCategories: () => dispatch(categoriesRdx.actions.reset()),
+    onCreateCategoryClick: () => {
+    },
+    onUpdateCategoryClick: () => {
+    }
+  })
 )(Categories));
