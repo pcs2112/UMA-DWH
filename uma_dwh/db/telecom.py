a271178ffd8908a35995dde20e91c4212e6bce14
Vicente Chavez <vchavez92780@gmail.com>
2019-09-03 21:53:10 -0400

    Fixed error handling sp call

--- uma_dwh/db/telecom.py
+++ uma_dwh/db/telecom.py
@@ -19,10 +19,11 @@ def execute_sp(*args, out_arg='sp_status_code'):
     :return: Stored procedure result sets and out argument
     :rtype: list
     """
-    results = execute_sp_with_required_in_args(*args, sp_args_length=8)
+    results = execute_sp_with_required_in_args(*args, sp_args_length=8, sp_in_arg_prefix='VARIABLE_')
+    print(results)
     status_code = get_out_arg(results, out_arg)
     
-    if status_code < 0:
+    if status_code == 0:
         raise SPException(f'Stored Procedure call to "{args[0]}" failed.', status_code)
     
     result = get_sp_result_set(results, 0, out_arg)
