6205d80ded92f85ff86dfdbc391867535583ed28
Vincent Chavez <vchavez92780@gmail.com>
2019-09-07 11:00:51 -0700

    Fixed typo

--- uma_dwh/db/telecom.py
+++ uma_dwh/db/telecom.py
@@ -7,7 +7,7 @@ def save_skill(skill_id, skill_update_type):
     """ Updates a skill. """
     execute_sp(
         'MWH_FILES.MANAGE_API_CONSOLE_DATA',
-        'UPDATE_SKILLS',
+        'UPDATE_SKILL',
         skill_id,
         skill_update_type
     )
