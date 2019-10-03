ba9af8ea55ca3939b52d04f9bff897c142ae99c3
Vincent Chavez <vchavez92780@gmail.com>
2019-10-02 22:49:24 -0400

    Added selector

--- src/redux/modules/dataLake/entries/selectors.js
+++ src/redux/modules/dataLake/entries/selectors.js
@@ -1,10 +1,16 @@
 import {
   createDataSelector,
-  createFetchingErrorSelector,
-  createGetItemsSelector
+  createFetchingErrorSelector, createGetItemByIdSelector,
+  createGetItemsSelector, createGetPropertySelector
 } from 'javascript-utils/lib/selectors';
+import { createSelector } from 'reselect/lib/index';
 
 const _getData = createDataSelector('dataLakeEntries', 'dataLoaded', 'data');
+
+/**
+ * Returns the updating entry id.
+ */
+const _getUpdatingEntryId = createGetPropertySelector('dataLakeEntries', 'updating');
 
 /**
  * Returns the error from the state.
@@ -16,3 +22,24 @@ export const getFetchingError = createFetchingErrorSelector('dataLakeEntries', '
  * Returns the entries data.
  */
 export const getData = createGetItemsSelector(_getData);
+
+/**
+ * Returns the current updating entry.
+ */
+export const getUpdatingEntry = createGetItemByIdSelector(_getData, _getUpdatingEntryId);
+
+/**
+ * Gets the initial form values for the updating entry.
+ */
+export const getUpdatingEntryInitialValues = createSelector(
+  [getUpdatingEntry],
+  (entry) => {
+    if (!entry) {
+      return {};
+    }
+
+    return {
+      entry_id: entry.id
+    };
+  }
+);
