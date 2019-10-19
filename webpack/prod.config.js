04fe987aab342f5669fee743cc028bc10c094536
Vicente Chavez <vchavez92780@gmail.com>
2019-10-19 14:13:54 -0400

    updated webpack

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -2,7 +2,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-const CleanPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const config = require('../src/config');
 
@@ -194,9 +194,7 @@ module.exports = {
     }
   },
   plugins: [
-    new CleanPlugin([resolve('uma_dwh/static/dist')], {
-      root: rootPath
-    }),
+    new CleanWebpackPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"production"'
