5c72ff511a3243de2f2ce10edaced2c34062ed33
Vicente Chavez <vchavez92780@gmail.com>
2019-10-19 12:15:49 -0400

    Updated npm packages

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -63,8 +63,9 @@ module.exports = {
           {
             loader: 'css-loader',
             options: {
-              modules: true,
-              localIdentName: '[local]___[hash:base64:5]',
+              modules: {
+                localIdentName: '[local]___[hash:base64:5]',
+              },
               sourceMap: true
             }
           },
