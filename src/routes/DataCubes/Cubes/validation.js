e85003efe932095a44a4e61608fed336b5d71c14
Vicente Chavez <vchavez92780@gmail.com>
2019-10-27 23:09:36 -0400

    Added cube form validation

---
+++ src/routes/DataCubes/Cubes/validation.js
@@ -0,0 +1,11 @@
+import memoize from 'lru-memoize';
+import { createValidator, required } from 'javascript-utils/lib/validation';
+
+export const cubeValidator = memoize(10)(createValidator({
+  cube_name: required(),
+  primary_fact_table: required(),
+  view_name: required(),
+  table_name: required(),
+  cube_date_start: required(),
+  cube_date_end: required()
+}));
