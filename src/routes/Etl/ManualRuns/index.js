0ceb55b2adb03d433e5c9ed86722ee8a18768e2d
Vicente Chavez <vchavez92780@gmail.com>
2019-11-13 16:33:58 -0500

    Fixed directory

--- src/routes/Etl/ManualRuns/index.js
+++ src/routes/Etl/ManualRuns/index.js
@@ -1,9 +1,38 @@
-import ManualRuns from './ManualRuns';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Segment } from 'semantic-ui-react';
+import withMainLayout from '../../../components/WithMainLayout';
+import globalCss from '../../../css/global';
 
-export default () => [
-  {
-    path: '/etl/runs/manual',
-    component: ManualRuns,
-    exact: true
+class ManualRuns extends Component {
+  static propTypes = {
+    fetchData: PropTypes.func.isRequired
+  };
+
+  componentDidMount() {
+    const { fetchData } = this.props;
+    fetchData();
   }
-];
+
+  render() {
+    return (
+      <div>
+        <Segment style={globalCss.pageHeaderSegment}>
+          <h1 style={globalCss.pageHeaderSegmentH1}>
+            UMA ETL Manual Runs
+          </h1>
+        </Segment>
+      </div>
+    );
+  }
+}
+
+export default withMainLayout(connect(
+  null,
+  () => ({
+    fetchData: () => {
+      Promise.all([]);
+    }
+  })
+)(ManualRuns));
---
+++ src/routes/Etl/index.js
@@ -0,0 +1,9 @@
+import ManualRuns from './ManualRuns';
+
+export default () => [
+  {
+    path: '/etl/runs/manual',
+    component: ManualRuns,
+    exact: true
+  }
+];
